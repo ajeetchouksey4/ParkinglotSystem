586df10adbf8386f0899af361e7ec326673ec21e
ajeetchouksey4 <ajeetchouksey4@gmail.com>
2021-09-13 03:43:27 +0530

    txt added

--- src/test/java/com/mongoapps/parkinglot/InputParserTest.java
+++ src/test/java/com/mongoapps/parkinglot/InputParserTest.java
@@ -1,9 +1,35 @@
 package com.mongoapps.parkinglot;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
- * Created by Ajeet Chouksey
+ * @author Ajeet Chouksey
  */
 public class InputParserTest {
-    
+    FetchInput inputParser = new FetchInput();
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+    @Test
+    public void parseTextInput() throws Exception {
+        inputParser.parseTextInput("hello");
+        assertEquals("Invalidinput", outContent.toString().trim().replace(" ", ""));
+        inputParser.parseTextInput("status");
+        assertEquals("Invalidinput\nSorry,parkinglotisnotcreated", outContent.toString().trim().replace(" ", ""));
+    }
 
 }
