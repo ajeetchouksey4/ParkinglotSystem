38fc9e4103173422762dd1e30967b82baa79c6b6
ajeetchouksey4 <ajeetchouksey4@gmail.com>
2021-09-13 00:50:51 +0530

    Developing User Input functionality

---
+++ src/main/java/com/mongoapps/parkinglot/FetchInput.java
@@ -0,0 +1,10 @@
+package com.mongoapps.parkinglot;
+
+public class FetchInput {
+
+	public void parseTextInput(String trim) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/main/java/com/mongoapps/parkinglot/Main.java
+++ src/main/java/com/mongoapps/parkinglot/Main.java
@@ -1,5 +1,9 @@
 package com.mongoapps.parkinglot;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @author Ajeet Chouksey
  * @version 1.0.0
@@ -7,23 +11,74 @@ package com.mongoapps.parkinglot;
  *
  */
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("\n\n\n\n\n");
-		System.out.println("===================================================================");
-		System.out.println("=================== Welcome to ParkingLot System ====================");
-		System.out.println("===================================================================");
+	static FetchInput inputParser = new FetchInput();
 
-		showMessage();
+	public static void main(String[] args) {
+
+		showWelcomeMessage();
+		showInputRefMessage();
+
+		switch (args.length) {
+		case 0:
+			System.out.println("to exit! Please enter 'exit' anytime");
+
+			// Running an infinite loop to fetch input as per user requirements
+			for (;;) {
+				try {
+					BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+					String inputString = bufferRead.readLine();
+					if (inputString.equalsIgnoreCase("exit")) {
+						break;
+					} else if ((inputString == null) || (inputString.isEmpty())) {
+						// Do nothing
+					} else {
+						inputParser.parseTextInput(inputString.trim());
+					}
+				} catch (IOException e) {
+					System.out.println("Oops! Error in reading the input from console.");
+					e.printStackTrace();
+				}
+			}
+			break;
+		case 1:
+			// File input/output
+			// FetchInput.parseFileInput(args[0]);
+			break;
+		default:
+			System.out.println("Invalid input");
+		}
 
 	}
 
-	private static void showMessage() {
+	private static void showWelcomeMessage() {
+		System.out.println("\n\n\n\n\n");
+		System.out.println("===================================================================");
+		System.out.println("=================== Welcome to ParkingLot System ====================");
+		System.out.println("**************  The New System Towards Digital Parking *************");
+		System.out.println("===================================================================");
+
+	}
+
+	private static void showInputRefMessage() {
 		StringBuffer buffer = new StringBuffer();
 		buffer = buffer.append("--------------Please Ensure the pattern of your input -----------------------")
 				.append("\n");
-		buffer = buffer.append("A) For creating parking lot of size n               ---> create_parking_lot {capacity}")
+		buffer = buffer.append("1) For creating parking lot of size n               ---> create_parking_lot {capacity}")
 				.append("\n");
-	
+		buffer = buffer
+				.append("2) To park a car                                    ---> park <<car_number>> {car_clour}")
+				.append("\n");
+		buffer = buffer.append("3) Remove car from parking                  ---> leave {slot_number}").append("\n");
+		buffer = buffer.append("4) Print status of parking slot                     ---> status").append("\n");
+		buffer = buffer.append(
+				"5) Get cars registration no for the given car color ---> registration_numbers_for_cars_with_color {car_color}")
+				.append("\n");
+		buffer = buffer.append(
+				"6) Get slot numbers for the given car color         ---> slot_numbers_for_cars_with_color {car_color}")
+				.append("\n");
+		buffer = buffer.append(
+				"7) Get slot number for the given car number         ---> slot_number_for_registration_number {car_number}")
+				.append("\n");
 		System.out.println(buffer.toString());
 	}
 
