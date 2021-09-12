c63c9334041a301f47a74c49225ffd6c03b64c7a
ajeetchouksey4 <ajeetchouksey4@gmail.com>
2021-09-13 02:11:59 +0530

    Creating functionality for creating parking lot

--- src/main/java/com/mongoapps/parkinglot/ParkingLot.java
+++ src/main/java/com/mongoapps/parkinglot/ParkingLot.java
@@ -1,5 +1,9 @@
 package com.mongoapps.parkinglot;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Ajeet Chouksey
  *
@@ -16,5 +20,48 @@ public class ParkingLot {
 			this.regNo = regNo;
 			this.color = color;
 		}
+	}
+
+	// availableSlotList: this is used to check for available slot list
+	ArrayList<Integer> availableSlotList;
+	//map1: for combination for slot with Car
+	Map<String, Car> map1;
+	//map2: for combination for regNo with slot
+	Map<String, String> map2;
+	//map2: for combination for Color with List of RegNo
+	Map<String, ArrayList<String>> map3;
+
+	public void createParkingLot(String lotCount) {
+		try {
+			this.MAX_SIZE = Integer.parseInt(lotCount);
+		} catch (Exception e) {
+			System.out.println("Invalid lot count");
+			System.out.println();
+		}
+		this.availableSlotList = new ArrayList<Integer>() {
+		};
+		for (int i = 1; i <= this.MAX_SIZE; i++) {
+			availableSlotList.add(i);
+		}
+		this.map1 = new HashMap<String, Car>();
+		
+	}
+
+	public void park(String regNo, String color) {
+	}
+
+	public void leave(String slotNo) {
+	}
+
+	public void status() {
+	}
+
+	public void getRegistrationNumbersFromColor(String color) {
+	}
+
+	public void getSlotNumbersFromColor(String color) {
+	}
+
+	public void getSlotNumberFromRegNo(String regNo) {
 	}
 }
