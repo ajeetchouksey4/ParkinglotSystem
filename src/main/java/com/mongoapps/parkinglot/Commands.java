b5d7df697fb13ac3ae704409db1db99731753b3b
ajeetchouksey4 <ajeetchouksey4@gmail.com>
2021-09-13 02:57:06 +0530

    Added commands for remainning functionality

--- src/main/java/com/mongoapps/parkinglot/Commands.java
+++ src/main/java/com/mongoapps/parkinglot/Commands.java
@@ -11,17 +11,24 @@ public class Commands {
 		commandsMap = new HashMap<String, Method>();
 
 		try {
-			generateCustomCommands();
+			commands();
 		} catch (NoSuchMethodException e) {
 			e.printStackTrace();
 
 		}
 	}
 
-	private void generateCustomCommands() throws NoSuchMethodException {
+	private void commands() throws NoSuchMethodException {
 		commandsMap.put("create_parking_lot", ParkingLot.class.getMethod("createParkingLot", String.class));
-		
-
+		commandsMap.put("park", ParkingLot.class.getMethod("park", String.class, String.class));
+		commandsMap.put("leave", ParkingLot.class.getMethod("leave", String.class));
+		commandsMap.put("status", ParkingLot.class.getMethod("status"));
+		commandsMap.put("registration_numbers_for_cars_with_colour",
+				ParkingLot.class.getMethod("getRegistrationNumbersFromColor", String.class));
+		commandsMap.put("slot_numbers_for_cars_with_colour",
+				ParkingLot.class.getMethod("getSlotNumbersFromColor", String.class));
+		commandsMap.put("slot_number_for_registration_number",
+				ParkingLot.class.getMethod("getSlotNumberFromRegNo", String.class));
 	}
 
 }
